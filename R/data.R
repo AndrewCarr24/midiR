25ed892afa41f4bd6ab44b4bbdb3aff4d2e1584f
Andrew Crar <andrew.carr@duke.edu>
2020-03-10 23:17:17 -0400

    Documentation for instruments_df data

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' Instrument names and hex codes of General MIDI
+#'
+#' @source Wikipedia. Unlabeled table - General MIDI
+#'  \url{https://en.wikipedia.org/wiki/General_MIDI}
+#' @format A data frame with 2 columns:
+#' \describe{
+#'  \item{instrument}{Instrument name}
+#'  \item{hex}{Hex code}
+#' }
