8cf5feebba02084d36e95d4ec02a3bd7b24bdc3b
Andrew Crar <andrew.carr@duke.edu>
2020-03-13 13:43:46 -0400

    Added random_glide function

---
+++ R/random_glide.R
@@ -0,0 +1,51 @@
+random_glide <-  function(seq_arg = NULL, prob = 1, position = "all", track_apply = NULL){
+
+  if(is.null(seq_arg) | seq_arg[1] == "rand_seq"){
+    obj <- as.list(match.call())
+    obj$func <- sys.function()
+    return(obj)
+  }
+
+  # Handling non-list seq arguments
+  if(!is.list(seq_arg)){
+    seq_arg <- list(seq_arg)
+  }
+
+  # Selecting tracks(s) to apply mutes to
+  track_arg = rep(TRUE, length(seq_arg))
+  if(!is.null(track_apply)){
+    track_arg = rep(FALSE, length(seq_arg))
+    track_arg[track_apply] = TRUE
+  }
+
+  lst <- purrr::map2(seq_arg, track_arg, function(seq, track){
+
+    # Dealing
+    if(!track){
+      return(seq)
+    }else{
+
+      mappings = template_and_builder_aux(seq = seq, position = position, prob = prob)
+
+      pos_mappings = mappings[[1]]
+      prob_mappings = mappings[[2]]
+
+      seq[pos_mappings] <- purrr::pmap(list(pos_mappings, prob_mappings), function(x, y){
+
+        if(length(seq) != x){
+          if(seq[x] != seq[x+1] & seq[x] != "rest" & seq[x+1] != "rest"){
+            sample(c(paste0(seq[x], "g"), seq[x]), 1, prob = c(y, 1-y))
+          }else{seq[x]}
+        }else{seq[x]}
+      }) %>% unlist
+
+      return(seq)
+    }
+  })
+
+ # if(length(lst) == 1){
+ #  return(lst[[1]])}else{
+      return(lst)
+  #  }
+
+}
